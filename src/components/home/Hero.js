078e5f28124084db0b9d890e144445d46d924a08
ArmsTina <didehddud1005@gmail.com>
2024-11-26 14:38:12 -0700

    Bug fixed

--- src/components/home/Hero.js
+++ src/components/home/Hero.js
@@ -1,6 +1,7 @@
-import React from 'react';
-import '../../styles/Hero.css';
-import Hero1 from '../../assets/restauranfood.jpg'
+import React from "react";
+import { Link } from "react-router-dom";
+import "../../styles/Hero.css";
+import Hero1 from "../../assets/restauranfood.jpg";
 
 function Hero() {
   return (
@@ -9,8 +10,13 @@ function Hero() {
         <div className="hero-text">
           <h2>Little Lemon</h2>
           <h3>Chicago</h3>
-          <p>We are a family owned Mediterranean restaurant, focused on traditional recipes served with a modern twist.</p>
-          <input type="button" value="Reserve a Table" />
+          <p>
+            We are a family owned Mediterranean restaurant, focused on
+            traditional recipes served with a modern twist.
+          </p>
+          <Link to="/reservations">
+            <input type="button" value="Reserve a Table" />
+          </Link>
         </div>
         <img src={Hero1} alt="Food" />
       </article>
